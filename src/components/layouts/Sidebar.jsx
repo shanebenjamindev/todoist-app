0fadc207ab35bce8ad546574f63533981b2ce092
Your Name <you@example.com>
2024-03-09 20:31:08 +0700

    first commit

---
+++ src/components/layouts/Sidebar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function Sidebar() {
+  return (
+    <>
+      <div className="bg-dark d-md-flex">
+        <img
+          url="https://th.bing.com/th/id/R.9ab7dbfdab6791b9679ac0e201b2cd2b?rik=pGJunoLwjFqlvA&pid=ImgRaw&r=0"
+          width={30}
+          height={30}
+          alt="user"
+        />
+
+    
+      </div>
+      <div></div>
+      <div></div>
+    </>
+  );
+}
